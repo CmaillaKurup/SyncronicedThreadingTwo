6e8b0aaa0c2b49fdf23663b63f22ad2c67a77257
CmaillaKurup <cami670v@zbc.dk>
2021-11-02 10:58:01 +0100

    Code done

--- SyncronicedThreadingTwo/Program.cs
+++ SyncronicedThreadingTwo/Program.cs
@@ -1,11 +1,58 @@
 ﻿using System;
+using System.Threading;
 
 namespace SyncronicedThreadingTwo
 {
+    //This Code prints 60 stars, then the count og chars, then a tag and the new count of tags and so it goes on
+    //This code uses monitor.enter/monito.exit as a lock to avoid deadpool
     class Program
     {
-        static void Main(string[] args)
+        private static int count = 0;
+        static Object _lock = new Object();
+        static void Main()
         {
+            Thread star = new Thread(PrintStar);
+            Thread tag = new Thread(PrintTag);
+            
+            star.Start();
+            tag.Start();
+        }
+
+        static void PrintStar()
+        {
+            while (true)
+            { 
+                Func('*');
+            }
+        }
+
+        static void PrintTag()
+        {
+            while (true)
+            {
+                Func('#');
+            }
+        }
+
+        static void Func(char symbol)
+        {
+            Monitor.Enter(_lock);
+            try
+            {
+                for (int i = 0; i < 60; i++)
+                {
+                    if (true)
+                    {
+                        Console.Write(symbol);
+                    }
+                }
+                Thread.Sleep(500);
+                Console.Write($"{count += 60}\n");
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
             
         }
     }
